78dc4f60591116d2b93cf36b1137dbe0019b65c6
Gilbert Röhrbein <payload.git@mailbox.org>
2022-06-19 18:34:16 +0200

    refactor tracing::instrument in subscriptions.rs

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -1,36 +1,36 @@
 use axum::{extract::Form, http::StatusCode, Extension};
 use sqlx::PgPool;
-use tracing::Instrument;
 
+#[tracing::instrument(skip_all, ret, fields(
+    request_id = %uuid::Uuid::new_v4(),
+    request = ?*req,
+))]
 pub async fn subscribe(req: Form<SubscribeRequest>, db: Extension<PgPool>) -> StatusCode {
-    let uuid = sqlx::types::Uuid::from_bytes(*uuid::Uuid::new_v4().as_bytes());
-    let span = tracing::info_span!("subscribe", %uuid, ?req, name = %req.name, email = %req.email);
-    let _guard = span.enter();
+    match db_insert_subscription(&req, &*db).await {
+        Ok(_) => StatusCode::OK,
+        Err(_) => StatusCode::INTERNAL_SERVER_ERROR,
+    }
+}
 
-    match sqlx::query!(
+#[tracing::instrument(skip_all, ret, err)]
+async fn db_insert_subscription(
+    request: &SubscribeRequest,
+    db: &PgPool,
+) -> Result<(), sqlx::Error> {
+    sqlx::query!(
         r#"insert into subscriptions (id, name, email, subscribed_at) values ($1, $2, $3, $4)"#,
-        uuid,
-        req.name,
-        req.email,
+        sqlx_uuid(),
+        request.name,
+        request.email,
         chrono::Utc::now()
     )
-    .execute(&*db)
-    .instrument(tracing::info_span!("subscribing"))
-    .await
-    {
-        Ok(_) => {
-            tracing::info!("subscribe done {uuid}");
-            StatusCode::OK
-        }
-        Err(err) => {
-            if let sqlx::Error::Database(pg_err) = err {
-                tracing::error!("subscribe error: {}", pg_err.message());
-            } else {
-                tracing::error!("subscribe error {err:?}");
-            }
-            StatusCode::INTERNAL_SERVER_ERROR
-        }
-    }
+    .execute(db)
+    .await?;
+    Ok(())
+}
+
+fn sqlx_uuid() -> sqlx::types::Uuid {
+    sqlx::types::Uuid::from_bytes(*uuid::Uuid::new_v4().as_bytes())
 }
 
 #[derive(serde::Deserialize, Debug)]
